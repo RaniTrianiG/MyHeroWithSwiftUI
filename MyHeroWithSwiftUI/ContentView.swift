f262ce14f80c4609c61968e5dbfb0c9be8b7ba5e
RaniTrianiG <ranitrianig@gmail.com>
2021-09-09 11:03:36 +0700

    Initial Commit

---
+++ MyHeroWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyHeroWithSwiftUI
+//
+//  Created by Rani Triani Gustia on 09/09/21.
+//  Copyright © 2021 Dicoding Indonesia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
